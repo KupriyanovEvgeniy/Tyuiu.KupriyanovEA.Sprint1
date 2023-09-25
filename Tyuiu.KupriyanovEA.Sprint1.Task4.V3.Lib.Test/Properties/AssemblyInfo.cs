50d7383c7528937dca67a51f7f7d66a62d7f2579
Tyuiu.KupriyanovEA@outlook.com <Tyuiu.KupriyanovEA@outlook.com>
2023-09-25 15:02:37 +0500

    Task4

---
+++ Tyuiu.KupriyanovEA.Sprint1.Task4.V3.Lib.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KupriyanovEA.Sprint1.Task4.V3.Lib.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KupriyanovEA.Sprint1.Task4.V3.Lib.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b7dca1d-4f3e-48f6-a4be-364f99722a73")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KupriyanovEA.Sprint1.Task4.V3.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KupriyanovEA.Sprint1.Task4.V3.Lib
+{
+    public class DataService : ISprint1Task4V3
+    {
+        public double Calculate(double x, double y)
+        {
+            if (x - y == 0)
+            {
+                Console.WriteLine("На ноль делить нельзя");
+                return 0;
+            }
+            else
+            {
+                var res = Math.Round(Math.Sqrt(x * 3) / Math.Pow(x - y, 2), 3);
+                Console.WriteLine("sqrt(x*3)/(x-y)^2 = ");
+                return res;
+            }
+            
+        }
+    }
+}
